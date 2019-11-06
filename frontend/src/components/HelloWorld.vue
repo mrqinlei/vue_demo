c7f3d62d8922b61454eab85edf92ce11d8c0136a
qinlei <mrqinlei@gmail.com>
2019-11-06 19:18:46 +0800

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- 这里router-link to="newcontact"会把路由导航到http://localhost:8080/#/newcontact   -->
+    <router-link to="newcontact"><h1>{{ msg }}</h1></router-link>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcom'
+    }
+  }
+}
+</script>
+<style>
+h1 {
+  font-weight:normal;
+}
+a {
+  color:#42b983;
+}
+</style>
